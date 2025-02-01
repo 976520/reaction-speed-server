46281a268cc9c898b47fd85e455134d9a5d95fcd
송재욱 <haensol976520@gmail.com>
2025-02-02 02:36:58 +0900

    fix-migration-bug

--- configs/settings.py
+++ configs/settings.py
@@ -79,9 +79,16 @@ WSGI_APPLICATION = "configs.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reaction_speed',     
+        'USER': 'root',         
+        'PASSWORD': '1234',      
+        'HOST': 'localhost',             
+        'PORT': '3306',                 
+        'OPTIONS': {
+            'charset': 'utf8mb4'
+        }
     }
 }
 
