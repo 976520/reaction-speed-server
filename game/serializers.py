a9993766497bf74c9d9009aac8ac00e4018eecec
송재욱 <haensol976520@gmail.com>
2025-02-02 16:40:22 +0900

    pw key

--- game/serializers.py
+++ game/serializers.py
@@ -4,17 +4,24 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+    
     class Meta:
         model = User
-        fields = ('id', 'username', 'nickname', 'total_games', 'wins', 'best_reaction_time')
+        fields = ('id', 'username', 'password', 'nickname', 'total_games', 'wins', 'best_reaction_time')
+        read_only_fields = ('id', 'total_games', 'wins', 'best_reaction_time')
         extra_kwargs = {
             'password': {'write_only': True}
         }
 
     def create(self, validated_data):
+        username = validated_data.get('username')
+        password = validated_data.get('password')
+        nickname = validated_data.get('nickname', username)  
+        
         user = User.objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['password'],
-            nickname=validated_data.get('nickname', '')
+            username=username,
+            password=password,
+            nickname=nickname
         )
         return user 
